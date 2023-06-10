a8a7a0463a3810066d2917ff035d6084c4fbb9c8
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-10 18:45:09 +0900

    6/10

--- practice.py
+++ practice.py
@@ -1,8 +1,4 @@
-terms = ["Z 3", "D 5"]
+files = [(0, 1), (1, 2), (2, 3)]
 
-terms_dict = {} # 약관 종류와 유효기간을 딕셔너리로 저장
-for i in terms:
-    term, expiration_date = i.split()
-    terms_dict[term] = expiration_date
-
-print(int(terms_dict['Z']) > 2)
+for i in range(len(files)):
+    print(files[i][1])
