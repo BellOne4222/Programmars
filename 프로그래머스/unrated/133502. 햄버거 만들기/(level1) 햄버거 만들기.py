b85fcb7aa8793efe7bda5c34a411d986fdfd53d5
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-13 18:02:46 +0900

    6/13

--- 프로그래머스/unrated/133502. 햄버거 만들기/(level1) 햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/(level1) 햄버거 만들기.py
@@ -3,7 +3,7 @@ def solution(ingredient):
     kitchen = []
     count = 0
     for i in ingredient:
-        kitchen.append(i)
+        kitchen.append(i) # 스택에 하나씩 넣으면서 햄버거 배열과 같아지면 같은 4개 pop 처리해서 같을때마다 햄버거 개수 1개 늘리면서 개수 반환하는 방식
         if kitchen[-4:] == hamburger:
             count += 1
             for j in range(4):
