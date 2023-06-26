6f7ce51fc76451878013e89bfea253e3309a5420
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-26 15:03:50 +0900

    6/26

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,6 +1,6 @@
 def solution(s):
-    stack = []
-    for i in s:
+    stack = [] # 스택 사용
+    for i in s: # 스택이 비면 i 넣고 다음 i와 stack의 마지막 값이 같으면 pop 처리
         
         if len(stack) == 0:
             stack.append(i)
@@ -14,7 +14,7 @@ def solution(s):
     
     
     
-    if len(stack) == 0:
+    if len(stack) == 0: # 짝이 다지어져서 stack에 아무것도 없으면 1, 짝이 안지어져서 남아있다면 0 반환
         result = 1
     elif len(stack) > 0:
         result = 0
