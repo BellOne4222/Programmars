b8f0481b327903962051fb9be03600576dc0be85
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-26 15:06:25 +0900

    6/26

--- practice.py
+++ practice.py
@@ -1,33 +1,13 @@
-def solution(dartResult):
-    answer = 0
-    scores = []
-    for i in dartResult:
-        if i.isdigit() == True:
-            score = int(i)
-            if i.isdigit() == True and dartResult[dartResult.index(i)+1].isdigit() == True:
-                a = (i+dartResult[dartResult.index(i)+1])
-                score = int(a)
-        elif i == "S":
-            scores.append(pow(score, 1))
-        elif i == "D":
-            scores.append(pow(score, 2))
-        elif i == "T":
-            scores.append(pow(score, 3))
-        elif i == "#":
-            scores[-1] *= -1
-        elif i == "*":
-            if len(scores) <= 1:
-                scores[-1] *= 2
-            else:
-                scores[-1] *= 2
-                scores[-2] *= 2
-                
-    for k in scores:
-        answer += k
-    return answer
-        
-                
-                    
-                
-                
-            
+def solution(n):
+    nums = [num for num in range(1, n+1)]
+    result = 0
+    for i in range(1, n+1):
+        for j in range(i, n+1):
+            sum = 0
+            while True:
+                sum += nums[j]
+                if sum == n:
+                    result += 1
+                    break
+    
+    return result
