fc8f4cf97c89971af5faa9c98be0e2f4b8e4a337
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-29 18:44:00 +0900

    6/29

--- practice.py
+++ practice.py
@@ -1,41 +1,20 @@
-# 문제 아이디어를 생각 할 때 가장 무거운 사람과 가벼운 사람부터 태우면서 확인해보자라는 생각으로 접근
-# 투 포인터 방식으로 구현을 하였고 가장 가벼운 사람과 무거운 사람을 더했을 때 limit보다 작거나 같으면 boat 하나 사용하고 가벼운 사람 인덱스는 1증가 시키고 무거운 사람 인덱스는 1감소시켜서 교차 될때 까지 반복
-# limit 보다 크면 보트에 한명만 탈 수 있으므로 가장 무거운 사람 인덱스를 1 감소 시키고 보트 수 1 증가
+# popleft를 사용하기 위해 deque를 사용을 했고, people을 정렬을 하고, 가장 무게가 무거운 사람을 빼서 이 사람과 같이 탈 수 있는 사람이 있는지 없는지 검사를 해서 없으면 보트 하나 증가 시키고, 
+# 보트 최대 무게에서 무거운 사람의 무게를 뺀 값이 가장 가벼운 사람보다 크거나 같으면 둘 다 보트에 태워서 deque에서 제거하여 보트 개수 반환하는 로직
+
+from collections import deque
 
 def solution(people, limit):
+    people = deque(sorted(people)) # deque 정렬
     boat = 0
-    people = sorted(people)
-    
-    LW_idx = 0 # 가장 무게가 가벼운 사람의 인덱스
-    HW_idx = len(people) - 1 # 가장 무게가 무거운 사람의 인덱스
-    
-    while LW_idx < HW_idx:
-        # 보트에 두 명을 태우는 경우
-        if people[LW_idx] + people[HW_idx] <= limit:
-            LW_idx += 1
-            HW_idx -= 1
-        # 보트에 한 명 밖에 못 태우는 경우
-        else:
-            HW_idx -= 1
-            
-        boat += 1
+
+    while people:
+        High_Weight = people.pop() # 가장 무거운 사람 pop
+        another_person = limit - High_Weight # 같이 보트에 탈 수 있는 사람의 무게를 보트 최대 무게 - 가장 무거운 사람의 무게
+        if people and (another_person >= people[0]): # 보트에 같이 탈수 있으면
+            people.popleft() # 둘 다 deque에서 제거하고 보트 수 1 증가
         
-    if LW_idx == HW_idx: # 같은 숫자에서 인덱스가 만났을 경우
-        boat += 1
-    
+        boat += 1 # 혼자만 탑승 가능하면 보트 수 1 증가
+        
     return boat
 
-
 print(solution([70, 50, 80, 50], 100))
-
-            
-                
-                
-        
-        
-            
-        
-    
-
-        
-        
