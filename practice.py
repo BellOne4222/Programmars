12f74b5bd1c09dd592adacb5a632894b8a179b15
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-03 17:12:33 +0900

    7/3

--- practice.py
+++ practice.py
@@ -1,20 +1,34 @@
-# popleft를 사용하기 위해 deque를 사용을 했고, people을 정렬을 하고, 가장 무게가 무거운 사람을 빼서 이 사람과 같이 탈 수 있는 사람이 있는지 없는지 검사를 해서 없으면 보트 하나 증가 시키고, 
-# 보트 최대 무게에서 무거운 사람의 무게를 뺀 값이 가장 가벼운 사람보다 크거나 같으면 둘 다 보트에 태워서 deque에서 제거하여 보트 개수 반환하는 로직
+def solution(k, tangerine):
+    arr = list(set(tangerine))
+    tan_dict = {i:0 for i in arr}
+    for j in arr:
+        tan_dict[j] = tangerine.count(j)
+    
+    tanger_dict = dict(sorted(tan_dict.items(), key=lambda x: x[1], reverse = True))
+    
+    tangerines = 0
+    result = 0
+    while True:
+        for l in tanger_dict:
+            tangerines += tanger_dict[l]
+            result += 1
+        if tangerines >= k:
+            break
+    return result
 
-from collections import deque
+from collections import Counter
 
-def solution(people, limit):
-    people = deque(sorted(people)) # deque 정렬
-    boat = 0
-
-    while people:
-        High_Weight = people.pop() # 가장 무거운 사람 pop
-        another_person = limit - High_Weight # 같이 보트에 탈 수 있는 사람의 무게를 보트 최대 무게 - 가장 무거운 사람의 무게
-        if people and (another_person >= people[0]): # 보트에 같이 탈수 있으면
-            people.popleft() # 둘 다 deque에서 제거하고 보트 수 1 증가
-        
-        boat += 1 # 혼자만 탑승 가능하면 보트 수 1 증가
-        
-    return boat
-
-print(solution([70, 50, 80, 50], 100))
+def solution(k, tangerine):
+    tan_counter = Counter(tangerine)
+    tanger_dict = dict(sorted(tan_counter.items(), key=lambda x: x[1], reverse=True))
+    
+    tangerines = 0
+    result = 0
+    for tangerine, count in tanger_dict.items():
+        if tangerines >= k:
+            break
+        tangerines += count
+        result += 1
+    
+    return result
+print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
