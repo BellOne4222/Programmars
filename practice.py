1a5d3e04590b0ea7e79106042da4fc3f713aaae7
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-13 19:51:05 +0900

    7/13

--- practice.py
+++ practice.py
@@ -1,6 +1,4 @@
 N = 4
-
-T, S = list(map(int, input().split()))
 Do_lst = [[3,5], [8,14], [5,20], [1,16]]
-Do_lst.sort(key=lambda x:x[0])
+Do_lst.sort(key=lambda x:x[1], reverse=True) # [[3, 5], [8, 14], [1, 16], [5, 20]]
 print(Do_lst)
