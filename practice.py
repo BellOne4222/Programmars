8cfc6eb9064f2970db6929fb9fd9f75cc9215be9
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-13 19:56:22 +0900

    7/13

--- practice.py
+++ practice.py
@@ -1,4 +1,17 @@
-N = 4
-Do_lst = [[3,5], [8,14], [5,20], [1,16]]
-Do_lst.sort(key=lambda x:x[1], reverse=True) # [[3, 5], [8, 14], [1, 16], [5, 20]]
-print(Do_lst)
+N = int(input())
+To_Do_lst= []
+for _ in range(N):
+    t, s = map(int, input().split())
+    To_Do_lst.append((s, t))
+To_Do_lst.sort(reverse=True)
+Wake_Limit = To_Do_lst[0][0] - To_Do_lst[0][1]
+for i in range(1, N):
+    if Wake_Limit > To_Do_lst[i][0]:
+        Wake_Limit = To_Do_lst[i][0] - To_Do_lst[i][1]
+    else:
+        Wake_Limit -= To_Do_lst[i][1]
+if Wake_Limit >= 0:
+    pass
+else:
+    Wake_Limit = -1
+print(Wake_Limit)
