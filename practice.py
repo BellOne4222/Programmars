71fe00c08d4d6998a22bd114d8db1915ef3ed249
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-17 18:14:24 +0900

    7/17

--- practice.py
+++ practice.py
@@ -1,66 +1,25 @@
-from collections import deque
-
-# 시간을 str에서 int형태로 변환하는 함수
-def get_int(plan):
-    subject, start, playtime = plan
-    
-    a, b = start.split(":")
-    
-    start = int(a)*60 + int(b)
-    
-    playtime = int(playtime)
-    
-    return subject, start, playtime
-
 def solution(plans):
-    answer = []
-    
-    # 시간 순서대로 정렬
-    plans.sort(key = lambda x : x[1])
 
-    st = []
-    
-    plans_idx = 1
-    
-    now_subject, now_start, now_playtime = get_int(plans[0])
-  
-    while 1:
-        next_subject, next_start, next_playtime = get_int(plans[plans_idx])
-        
-        # 새로운 과제 시작시간이 진행 중이던 과제 끝나는 시간보다 빠르면 -> 진행 중 과제 멈추고 새로운 과제 시작
-        if next_start < now_start + now_playtime:
-            st.append((now_subject, now_start + now_playtime - next_start))
-            now_subject, now_start, now_playtime = next_subject, next_start, next_playtime
-            plans_idx += 1
-            
-        # 그렇지 않다면 진행 중 과제 마치고. 멈춰둔 과제 or 새로 시작해야 하는 과제해야되는지 결정
-        else:
-            answer.append(now_subject)
-              
-            # 멈춰둔 과제가 있는 경우
-            if st:
-                # 과제를 끝낸 시각에 새로 시작해야 되는 과제와 잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야하는 과제부터 진행
-                if next_start == now_start + now_playtime:
-                    now_subject, now_start, now_playtime = next_subject, next_start, next_playtime
-                    plans_idx += 1     
-                else:
-                    subject, remain_playtime = st.pop()
-                    now_subject, now_start, now_playtime = subject, now_start + now_playtime, remain_playtime
-                     
-            # 멈춰둔 과제가 없다면 새로 시작해야 하는 과제 시작
-            else:
-                now_subject, now_start, now_playtime = next_subject, next_start, next_playtime
-                plans_idx += 1 
-        
-        # 새로 배울게 없다면 진행중인 과제 마무리
-        if plans_idx == len(plans):
-            answer.append(now_subject)
-            break
-    
-    # 최근에 멈춘 과제부터 마무리
-    for subject, _ in st[::-1]:
-        answer.append(subject)
- 
-    return answer
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1]) # [['history', 840, 30], ['science', 760, 50], ['computer', 750, 100], ['music', 740, 40]]
+    answer = []
+    while plans:
+        x = plans.pop()
+        for idx, val in enumerate(answer):
+            if val[0] > x[1]:
+                answer[idx][0] += x[2]
+        answer.append([x[1] + x[2], x[0]])
+    answer.sort()
+
+    return list(map(lambda x: x[1], answer))
 
 print(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
+
+# 입력된 "plans"를 처리하여 시간 순서대로 정렬합니다. 각 계획은 [과목명, 시작 시간(분), 소요 시간] 형식의 리스트로 변환됩니다. 이때, 시작 시간을 분 단위로 변환하여 계산합니다. (예: "12:40" -> 12 * 60 + 40 = 760)
+# 빈 리스트인 "answer"를 생성합니다.
+# "plans"가 비어있을 때까지 다음을 반복합니다:
+# "plans"에서 가장 늦게 시작하는 계획을 꺼내옵니다.
+# "answer"에 있는 각 계획의 시작 시간과 비교하여 해당 시간 이후에 시작하는 계획이라면, 해당 계획의 시작 시간을 더하고 시간이 겹치는 경우를 처리합니다.
+# 현재 계획을 "answer"에 추가합니다. [계획의 종료 시간, 과목명] 형식으로 저장됩니다.
+# "answer"를 시간 순서대로 정렬합니다.
+# "answer"에서 각 계획의 과목명만 추출하여 리스트로 변환합니다.
+# 변환된 리스트를 반환합니다.
