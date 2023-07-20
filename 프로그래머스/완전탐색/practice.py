2760e56cbe428786310b26fdae7e51b4e1149a38
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-20 19:53:20 +0900

    7/20

--- 프로그래머스/완전탐색/practice.py
+++ 프로그래머스/완전탐색/practice.py
@@ -1,17 +1,38 @@
-def solution(targets):
-    targets = sorted(targets)
-    misail = 1
-    current = targets[0]
-    start, end = current[0], current[1]
-    for i in range(1, len(targets)):
-        if targets[i][0] > end:
-            misail += 1
-            current = targets[i]
-            start, end = current[0], current[1]
-        else:
-            if targets[i][1] < end:
-                end = targets[i][1]
-    
-    return misail
+result = 10 ** 6
 
-print(solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]))
+
+def dfs(picks, minerals, fatigability):
+    global result
+
+    if sum(picks) == 0 or not minerals: # 곡괭이가 없거나 캘 광물이 없을 때, 피로도 반환
+        result = min(fatigability, result)
+        return
+
+    for i in range(len(picks)):
+        fatigue = 0
+        if picks[i] >= 1:
+            picks[i] -= 1
+            mined = minerals[:5]
+            if i == 0: # 다이아 곡괭이 일때
+                fatigue += len(mined)
+            elif i == 1: # 철 곡괭이 일때
+                for mineral in mined:
+                    if mineral == "diamond":
+                        fatigue += 5
+                    else:
+                        fatigue += 1
+            elif i == 2: # 돌 곡괭이 일때
+                for mineral in mined:
+                    if mineral == "diamond":
+                        fatigue += 25
+                    elif mineral == "iron":
+                        fatigue += 5
+                    else:
+                        fatigue += 1
+            dfs(picks, minerals[5:], fatigability + fatigue) # 광물 배열에서 5개 캤으므로 5부터 잘라서 다시 dfs 호출
+            picks[i] += 1 # 곡괭이를 하나 썼고 다음 광물 캘때의 곡괭이 조합에서 다시 사용할 수 있게 1개 다시 추가
+
+
+def solution(picks, minerals):
+    dfs(picks,minerals,0)
+    return result
