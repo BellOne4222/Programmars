a6b725fd61df11bc285b01af02de09a6ad596ff7
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-16 17:53:54 +0900

    8/16

--- prac.py
+++ prac.py
@@ -1,27 +1,45 @@
 from collections import deque
 
-def solution(skill, skill_trees):
-    essential_skills = [i for i in skill]
-    essential_skill = deque(essential_skills)
-    skill_sequence = [j for j in range(len(skill))]
-    skill_dict = dict(zip(essential_skill,skill_sequence))
-    available = 0
-    cnt = 0
+def solution(queue1, queue2):
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    sm_1 = sum(queue1)
+    sm_2 = sum(queue2)
+    all_sm = sm_1 + sm_2
+    target = int(all_sm / 2)
+    change = 0
+    all_queue = queue1+ queue2
+    cnt = len(all_queue)
+    p = 0 # pop 횟수
+    a = 0 # apeend 횟수
+    result = 0
     
-    for j in skill_trees:
-        cnt = 0
-        essential_skills = [i for i in skill]
-        essential_skill = deque(essential_skills)
-        for k in j:
-            if k not in skill_dict:
-                cnt += 1
-            elif k == essential_skill[0]:
-                cnt += 1
-                essential_skill.popleft()
-            else:
-                break
-        if cnt == len(j):
-            available += 1
-    return available
-
-print(solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]))
+    while cnt > 0:
+        if p == 1 and a == 1:
+            result += 1
+            p = 0
+            a = 0
+            
+        if sm_1 == target and sm_2 == target:
+            return result
+        
+        if sm_1 > sm_2:
+            change = queue1.popleft()
+            p += 1
+            queue2.append(change)
+            a += 1
+            sm_1 -= change
+            sm_2 += change
+            cnt -= 1
+        elif sm_1 < sm_2:
+            change = queue2.popleft()
+            p += 1
+            queue1.append(change)
+            a += 1
+            sm_1 += change
+            sm_2 -= change
+            cnt -= 1
+    return -1
+        
+            
+        
