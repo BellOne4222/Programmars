e9f3f3fbce95383611a9102db870d6a97b79d0a9
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-16 19:20:47 +0900

    8/16

--- prac.py
+++ prac.py
@@ -1,45 +1,63 @@
-from collections import deque
-
-def solution(queue1, queue2):
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    sm_1 = sum(queue1)
-    sm_2 = sum(queue2)
-    all_sm = sm_1 + sm_2
-    target = int(all_sm / 2)
-    change = 0
-    all_queue = queue1+ queue2
-    cnt = len(all_queue)
-    p = 0 # pop 횟수
-    a = 0 # apeend 횟수
-    result = 0
+def solution(topping):
     
-    while cnt > 0:
-        if p == 1 and a == 1:
-            result += 1
-            p = 0
-            a = 0
-            
-        if sm_1 == target and sm_2 == target:
-            return result
+    # 자르는 경우의 수
+    case = 0
+    
+    num = 0
+    # 1. 토핑 길이가 짝수일 때, 홀수일 때 경우 나눠서 반씩 나누기
+    
+    # 철수
+    elder = []
+    # 동생
+    younger = []
+    
+    new_elder = []
+    new_younger = []
+    
+    
+    # 짝수일 때
+    if len(topping) % 2 == 0:
+        num = int(len(topping) / 2)
+        elder = topping[0:num]
+        younger = topping[num:len(topping)]
+        print(num, elder, younger)
         
-        if sm_1 > sm_2:
-            change = queue1.popleft()
-            p += 1
-            queue2.append(change)
-            a += 1
-            sm_1 -= change
-            sm_2 += change
-            cnt -= 1
-        elif sm_1 < sm_2:
-            change = queue2.popleft()
-            p += 1
-            queue1.append(change)
-            a += 1
-            sm_1 += change
-            sm_2 -= change
-            cnt -= 1
-    return -1
+    # 2. 반으로 나눈 집합 2개 중복 제거
+        new_elder = list(set(elder))
+        new_younger = list(set(younger))
         
-            
+        if len(new_elder) == len(new_younger):
+            case += 1
+        elif len(new_elder) > len(new_younger):
+            pass
+        else:
+            pass
         
+        
+        
+    
+    # 홀수일 때
+    else:
+        num = int(len(topping) / 2)
+        elder = topping[0:num]
+        younger = topping[num:len(topping)]
+        print(num, elder, younger)
+        
+    # 2. 반으로 나눈 집합 2개 중복 제거
+        new_elder = list(set(elder))
+        new_younger = list(set(younger))
+        
+        if len(new_elder) == len(new_younger):
+            case += 1
+        elif len(new_elder) > len(new_younger):
+            pass
+        else:
+            pass
+        
+    
+    # 3. 중복 제거한 두 집합 각각의 원소 개수 비교해서 같아 질때까지 반복
+    
+    # 4. 중복 제거한 topping 길이가 홀수이면, break하고 0반환
+    
+    
+    
--- practice.py
+++ practice.py
@@ -1,65 +1,21 @@
-def solution(dirs):
-    start = (0,0)
-    up = (0,1)
-    down = (0,-1)
-    left = (-1,0)
-    right = (1,0)
-    before = start
-    check = []
-    sm = 0
-    road = 0
-    cnt = 0
+from collections import Counter
+
+def solution(topping):
+    elder = Counter(topping)
     
-    for i in range(len(dirs)):
-        if dirs[i] == 'U':
-            sm = tuple(sum(j) for j in zip(before, up))
-            if sm[0] > 5 or sm[0] < -5 or sm[1] > 5 or sm[1] < -5:
-                continue
-            road = (before,sm)
-            if road not in check:
-                check.append(road)
-                cnt += 1
-            else:
-                before = sm
-                continue
-            
-            
-        elif dirs[i] == 'D':
-            sm = tuple(sum(j) for j in zip(before, down))
-            if sm[0] > 5 or sm[0] < -5 or sm[1] > 5 or sm[1] < -5:
-                continue
-            road = (before,sm)
-            if road not in check:
-                check.append(road)
-                cnt += 1
-            else:
-                before = sm
-                continue
-        elif dirs[i] == 'L':
-            sm = tuple(sum(j) for j in zip(before, left))
-            if sm[0] > 5 or sm[0] < -5 or sm[1] > 5 or sm[1] < -5:
-                continue
-            road = (before,sm)
-            if road not in check:
-                check.append(road)
-                cnt += 1
-            else:
-                before = sm
-                continue
-        else:
-            sm = tuple(sum(j) for j in zip(before, right))
-            if sm[0] > 5 or sm[0] < -5 or sm[1] > 5 or sm[1] < -5:
-                continue
-            road = (before,sm)
-            if road not in check:
-                check.append(road)
-                cnt += 1
-            else:
-                before = sm
-                continue
-                
-        
-        before = sm
-        
-    return cnt
-print(solution("ULURRDLLU"))       
+    younger = set()
+    case = 0
+    
+    for i in topping:
+        younger.add(i)
+        elder[i] -= 1
+
+        if elder[i] == 0:
+            del elder[i]
+        if len(younger) == len(elder):
+            case += 1
+
+    return case
+
+
+print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
